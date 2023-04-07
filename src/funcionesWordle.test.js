b586b1a533691c909f95d352fd299b26d414ab4a
BMB <bruno.marquez@ucb.edu.bo>
2023-04-07 17:49:09 -0400

    Solved Test Function generarResultadoVacio(tamPalabraSecreta)

--- src/funcionesWordle.test.js
+++ src/funcionesWordle.test.js
@@ -1,14 +1,11 @@
 import FuncionesWordle from "./funcionesWordle"
-import WordleError from "./ExcepcionesWordle";
-import Wordle from "./wordle";
 
 describe("Funcion esIntentoIncompleto(intento, tamPalabraSecreta)", () => {
   let funcionesWordle = new FuncionesWordle();
   it('TCU 1. Debe lanzar un error si el intento es incompleto', () => {
-    Error = new WordleError("Palabra Incompleta.")
     expect(() => {
       funcionesWordle.esIntentoIncompleto('hola', 6);
-    }).toThrowError(Error);
+    }).toThrowError("Palabra Incompleta.");
   });
 
   it('TCU 2. No debe lanzar un error si el intento es completo', () => {
