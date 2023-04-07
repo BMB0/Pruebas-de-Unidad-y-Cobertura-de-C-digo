983ddaf011e93040ed8f57ccac46dd2f42b935ff
BMB <bruno.marquez@ucb.edu.bo>
2023-04-06 20:01:45 -0400

    [TCU 1]esIntentoIncompleto FROM funcionesWordle.js EFFORT(3)

--- src/funcionesWordle.test.js
+++ src/funcionesWordle.test.js
@@ -1,5 +1,12 @@
-import wordle from "./wordle.js"
+import FuncionesWordle from "./funcionesWordle"
+import WordleError from "./ExcepcionesWordle";
 
-describe("funcionesWordleTest", () => {
-        funWrodle = new wordle()
+describe("Funcion esIntentoIncompleto(intento, tamPalabraSecreta)", () => {
+  let funcionesWordle = new FuncionesWordle();
+  it('TCU 1. Debe lanzar un error si el intento es incompleto', () => {
+    Error = new WordleError("Palabra Incompleta.")
+    expect(() => {
+      funcionesWordle.esIntentoIncompleto('hola', 6);
+    }).toThrowError(Error);
+  });
 })
