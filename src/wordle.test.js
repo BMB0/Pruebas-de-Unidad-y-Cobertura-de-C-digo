87737560a93ef938120596924dcdd16c517733f6
BMB <bruno.marquez@ucb.edu.bo>
2023-04-11 19:57:32 -0400

    [TC1]function obtenerCategoriaSistemas FROM src/wordle.js Effort(1)

--- src/wordle.test.js
+++ src/wordle.test.js
@@ -6,3 +6,12 @@ describe("obtenerIntento()", () => {
       expect(wordle.obtenerIntento()).toBe(undefined);
     });
   })
+
+  describe('obtenerCategoriaSistemas()', () => {
+    it('TC1. debería retornar la lista de palabras de la categoría Sistemas', () => {
+      const wordle = new Wordle();
+      const resultadoEsperado = ["HOLA", "PERO"];
+      const resultadoObtenido = wordle.obtenerCategoriaSistemas();
+      expect(resultadoObtenido).toEqual(resultadoEsperado);
+    });
+  });
